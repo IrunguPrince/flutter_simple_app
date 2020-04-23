5c10ea9660d5739e13a8f519d1674528ea9cf796
Prince Irungu <princeirungu@gmail.com>
2020-04-23 10:06:17 +0300

    Final changes to the app. Theme and functionality

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,17 +3,31 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startProduct;
+  ProductManager(this.startProduct);
   @override
   _ProductManagerState createState() => _ProductManagerState();
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    _products.add(widget.startProduct);
+    super.initState();
+  }
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
   @override
   Widget build(BuildContext context) {
     return Column(children: [Container(
               margin: EdgeInsets.all(10.0),
               child:  RaisedButton(
+                color: Theme.of(context).primaryColor,
                 child: Text('Add Product'),
                 onPressed: (){
                   setState(() {
